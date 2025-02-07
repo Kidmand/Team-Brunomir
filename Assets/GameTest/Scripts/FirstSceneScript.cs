f223b7c40cbbee30c6e94db4cf4c7b31a2ca1ef1
kidmand <daian.gimenez@mi.unc.edu.ar>
2025-02-07 10:17:56 -0300

    Primer commit del proyecto Unity

---
+++ Assets/GameTest/Scripts/FirstSceneScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Doublsb.Dialog;
+
+public class FirstSceneScript : MonoBehaviour
+{
+    public DialogManager DialogManager;
+
+    private void Awake()
+    {
+        var dialogTexts = new List<DialogData>();
+
+        dialogTexts.Add(new DialogData("/size:up/Hi, /size:init/my name is Li.", "Li"));
+        dialogTexts.Add(new DialogData("Let's start this test!", "Li"));
+        dialogTexts.Add(new DialogData("The idea is to create something very simple, make me react to the text and animate me in a few different ways.", "Li"));
+        dialogTexts.Add(new DialogData("Remember that you can change my sprite or background if you choose so, you can even go 3D if you're more experienced with that!", "Li"));
+        dialogTexts.Add(new DialogData("Anyways... Let's move on!", "Li"));
+        dialogTexts.Add(new DialogData("Create a branching option where you have to choose between 3 possible answers to give me.", "Li"));
+
+        // Añadir opciones de selección
+        var selectionText = new DialogData("Choose an option:", "Li");
+        selectionText.SelectList.Add("Option A", "A");
+        selectionText.SelectList.Add("Option B", "B");
+        selectionText.SelectList.Add("Option C", "C");
+        selectionText.Callback = () => HandleSelection();
+        dialogTexts.Add(selectionText);
+
+        DialogManager.Show(dialogTexts);
+    }
+
+    private void HandleSelection()
+    {
+        var dialogTexts = new List<DialogData>();
+
+        if (DialogManager.Result == "A")
+        {
+            dialogTexts.Add(new DialogData("You chose option A!", "Li"));
+        }
+        else if (DialogManager.Result == "B")
+        {
+            dialogTexts.Add(new DialogData("You chose option B!", "Li"));
+        }
+        else if (DialogManager.Result == "C")
+        {
+            dialogTexts.Add(new DialogData("You chose option C!", "Li"));
+        }
+
+        dialogTexts.Add(new DialogData("After this, you'll send me to the SecondScene!", "Li"));
+        dialogTexts.Add(new DialogData("Where you'll have to create a VERY simple mini game and make me react to it.", "Li"));
+
+        DialogManager.Show(dialogTexts);
+    }
+}
