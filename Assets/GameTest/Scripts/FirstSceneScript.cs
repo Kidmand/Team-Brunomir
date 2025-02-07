3fb8452c36196062579b4de967200dd54f6fd45d
kidmand <daian.gimenez@mi.unc.edu.ar>
2025-02-07 16:16:26 -0300

    Se agregó un sprite de emoción sorpresa y se terminó la escena del diálogo entre los personajes y se corrigió errores ortográficos. También se modificaron un poco los los sprites de tristeza.

--- Assets/GameTest/Scripts/FirstSceneScript.cs
+++ Assets/GameTest/Scripts/FirstSceneScript.cs
@@ -10,16 +10,26 @@ public class FirstSceneScript : MonoBehaviour
     private void Awake()
     {
         var dialogTexts = new List<DialogData>();
-
-        dialogTexts.Add(new DialogData("/emote:Happy//size:up/Hola, /size:init/mi nombre es.", "Dante"));
-        dialogTexts.Add(new DialogData("/emote:Happy/Hola Dante, yo soy Kidmand. Encantado de conocerte!", "Kidmand"));
+        
+        // Diálogos entre los personajes y uso de emociones y reacciones en los sprites.
+        dialogTexts.Add(new DialogData("/emote:Happy/¡Hola, /size:init/mi nombre es Dante!.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Happy/Hola Dante, yo soy Kidmand. ¡Encantado de conocerte!", "Kidmand"));
         dialogTexts.Add(new DialogData("/emote:Happy/ El placer es mío.", "Dante"));
         dialogTexts.Add(new DialogData("Hay algo de lo que quiero hablarte Kidmand, quizá te resulte anormal pero considero que es muy importante saberlo.", "Dante"));
-        dialogTexts.Add(new DialogData("Oh! Por supuesto Dante, estoy curioso de saber de que se trata. Adelante, dime. ", "Kidmand"));
-        dialogTexts.Add(new DialogData("Tu consideras que el desarrollo personal es algo importante en nuestras vidas.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Sorpresa/¡Oh, por supuesto Dante!/wait:0.1//emote:Happy/ Estoy curioso de saber de que se trata, dime. ", "Kidmand"));
+        dialogTexts.Add(new DialogData("¿Tú consideras que el desarrollo personal es algo importante en nuestras vidas?.", "Dante"));
+        dialogTexts.Add(new DialogData("Siendo sincero, si considero que sea una parte fundamental en nuestras vidas.", "Kidmand"));
+        dialogTexts.Add(new DialogData("De hecho es tan importante que creo que en las universidades debería darse un curso al respecto.", "Kidmand"));
+        dialogTexts.Add(new DialogData("/emote:Sorpresa/¡Vaya! /wait:0.1//emote:Normal/Sabes que justo eso es algo que también creo yo.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Happy/Bueno, ya tenemos algo en común.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Happy/¡Definitivamente! /wait:0.1//emote:Normal/Y ya que preguntas sobre eso Dante, te tengo un pequeño juego. ¿Te animas a completarlo?.", "Kidmand"));
+        dialogTexts.Add(new DialogData("/emote:Sorpresa/¡Oh, un juego!... /wait:0.1//emote:Normal/¿De que se trataría?.", "Dante"));
+        dialogTexts.Add(new DialogData("/size:down/Te haré una pregunta sobre una frase referida al desarrollo personal y te daré tres opciones para elegir la correcta, ¿De Acuerdo?.", "Kidmand"));
+        dialogTexts.Add(new DialogData("/emote:Happy/Si, me encanta. Adelante, estoy listo.", "Dante"));
 
-        // Añadir opciones de selección
-        var selectionText = new DialogData("/size:down/Veamos querido lector si conoces a un famoso líder de desarrollo personal llamado Brian Tracy. En unos de sus maravillosos cursos (El seminario Fénix) dice una frase la cúal es:", "Li");
+
+        // Añadir opciones de selección. 
+        var selectionText = new DialogData("/size:down/Bueno, veamos si conoces a un famoso líder de desarrollo personal llamado Brian Tracy. En unos de sus maravillosos cursos (El seminario Fénix) dice una frase, ¿La cúal sería?:", "Kidmand");
         selectionText.SelectList.Add("Correct","Jamás, jamás, jamás se den por vencidos.");
         selectionText.SelectList.Add("Incorrect1","Lo que la mente del hombre puede concebir y creer, puede lograrse.");
         selectionText.SelectList.Add("Incorrect2","El éxito es una cuestión de motivación, creencias y acostumbrarse a la acción.");
@@ -29,25 +39,37 @@ public class FirstSceneScript : MonoBehaviour
         DialogManager.Show(dialogTexts);
     }
 
+
+        // Manejar la selección del jugador, y seguir pequeños diálogos entre los personajes. 
     private void HandleSelection()
     {
         var dialogTexts = new List<DialogData>();
 
         if (DialogManager.Result == "Correct")
         {
-            dialogTexts.Add(new DialogData("/emote:Happy/Felicitaciones, tienes una excelente memoria al recordar la frase correctamente o tiene suerte por haber adivinado ;).", "Dante"));
+            dialogTexts.Add(new DialogData("/emote:Sorpresa/ ¡Oh, felicitaciones!/wait:0.1//emote:Happy/ Tienes una excelente memoria al recordar la frase correctamente o tiene suerte por haber adivinado ;).", "Kidmand"));
+            dialogTexts.Add(new DialogData("/emote:Happy/ ¡Muchas Gracias! /wait:0.1//emote:Normal/Sí, vi el seminario fénix, me pareció extraodinario.", "Dante"));
         }
         else if (DialogManager.Result == "Incorrect1")
         {
-            dialogTexts.Add(new DialogData("/emote:Sad//size:down/Incorrecto, la frase fue dicha por el escritor Napoleón Hill (1883-1970). Uno de los autores de autoayuda y superación más prestigioso del mundo. ", "Dante"));
+            dialogTexts.Add(new DialogData("/size:down/Incorrecto, la frase fue dicha por el escritor Napoleón Hill (1883-1970). Uno de los autores de autoayuda y superación más prestigioso del mundo. ", "Kidmand"));
+            dialogTexts.Add(new DialogData("/emote:Sad/Supongo que tendré que seguir aprendiendo más.", "Dante"));
         }
         else if (DialogManager.Result == "Incorrect2")
         {
-            dialogTexts.Add(new DialogData("/emote:Sad//size:down/Incorrecto, la frase fue dicha por el escritor y empresario Dale Carnagie (1888-1955). Uno de sus libros más famoso es (Cómo ganar amigos e influír en las personas)." , "Dante"));
+            dialogTexts.Add(new DialogData("/size:down/Incorrecto, la frase fue dicha por el escritor y empresario Dale Carnagie (1888-1955). Uno de sus libros más famoso es (Cómo ganar amigos e influír en las personas)." , "Kidmand"));
+            dialogTexts.Add(new DialogData("/emote:Sad/Supongo que tendré que seguir aprendiendo más.", "Dante"));
         }
 
-        dialogTexts.Add(new DialogData("After this, you'll send me to the SecondScene!", "Dante"));
-        dialogTexts.Add(new DialogData("Where you'll have to create a VERY simple mini game and make me react to it.", "Dante"));
+
+        // En caso de que la respuesta sea incorrecta.    
+        if (DialogManager.Result != "Correct")
+        { 
+            dialogTexts.Add(new DialogData("Vamos, no te desanimes. Tampoco se pueden saber todas las frases del mundo.", "Kidmand"));
+            dialogTexts.Add(new DialogData("/emote:Happy/Tienes razon, de igual manera gracias por el juego. Me dieron más ganas de aprender y mejorar.", "Dante"));
+        }
+
+        dialogTexts.Add(new DialogData("/emote:Happy/Me alegra que hayas dicho eso.", "Kidmand"));
 
         DialogManager.Show(dialogTexts);
     }
