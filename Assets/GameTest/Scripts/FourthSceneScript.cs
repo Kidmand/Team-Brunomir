faa8b4779b792d3a755248ab97bd6f6c502aed74
kidmand <daian.gimenez@mi.unc.edu.ar>
2025-02-08 13:47:18 -0300

    Inócorporacin de nuevos spirtes y background

--- Assets/GameTest/Scripts/FourthSceneScript.cs
+++ Assets/GameTest/Scripts/FourthSceneScript.cs
@@ -11,7 +11,7 @@ public class FourthSceneScript : MonoBehaviour
     {
         var dialogTexts = new List<DialogData>();
 
-        dialogTexts.Add(new DialogData("Here I will inform you of the results in the previous Scenes.", "Li"));
+        dialogTexts.Add(new DialogData("Here I will inform you of the results in the previous Scenes.", "Dante"));
 
         dialogTexts.Add(new DialogData("You choose option A - B - C in the first scene.", "Li"));
 
--- Assets/GameTest/Scripts/ThirdSceneScript.cs
+++ Assets/GameTest/Scripts/ThirdSceneScript.cs
@@ -11,21 +11,20 @@ public class ThirdSceneScript : MonoBehaviour
     {
         var dialogTexts = new List<DialogData>();
 
-        dialogTexts.Add(new DialogData("Here I will inform you of the results in the previous Scenes.", "Li"));
+        dialogTexts.Add(new DialogData("Here I will inform you of the results in the previous Scenes.", "Dafne"));
 
-        dialogTexts.Add(new DialogData("You choose option A - B - C in the first scene.", "Li"));
+        dialogTexts.Add(new DialogData("You choose option A - B - C in the first scene.","Dante"));
 
-        dialogTexts.Add(new DialogData("The outcome of the mini-game was 1 - 2 - 3.", "Li"));
+        dialogTexts.Add(new DialogData("The outcome of the mini-game was 1 - 2 - 3.", "Dafne"));
 
-        dialogTexts.Add(new DialogData("So the result is of the game is A - B - C + 1 - 2 - 3.", "Li"));
+        dialogTexts.Add(new DialogData("So the result is of the game is A - B - C + 1 - 2 - 3.", "Dante"));
 
-        dialogTexts.Add(new DialogData("And that's basically it!", "Li"));
+        dialogTexts.Add(new DialogData("And that's basically it!", "Dafne"));
 
-        dialogTexts.Add(new DialogData("I hope you had fun in this little test! ", "Li"));
+        dialogTexts.Add(new DialogData("I hope you had fun in this little test! ", "Dante"));
 
-        dialogTexts.Add(new DialogData("I'm looking forward to see what you're capable of!", "Li"));
+        dialogTexts.Add(new DialogData("I'm looking forward to see what you're capable of!", "Dafne"));
 
-        
         DialogManager.Show(dialogTexts);
     }
 }
