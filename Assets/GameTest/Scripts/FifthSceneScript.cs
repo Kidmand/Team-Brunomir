d825ec2306e59b7d925c11f93eeae70b6471a790
kidmand <daian.gimenez@mi.unc.edu.ar>
2025-02-09 14:40:00 -0300

    Finalización de la sexta escena.

--- Assets/GameTest/Scripts/FifthSceneScript.cs
+++ Assets/GameTest/Scripts/FifthSceneScript.cs
@@ -14,10 +14,10 @@ public class FifthSceneScript : MonoBehaviour
 
         //Diálogos de los personajes Dafne y Dante en el Bosque.  
         dialogTexts.Add(new DialogData("/emote:Normal/Por cierto, ¿De dónde eres?.", "Dante"));
-        dialogTexts.Add(new DialogData("/size:down//emote:Normal/No soy de por aquí, de hecho hace poco que vine a la ciudad por lo que no conozco mucho. Además, me gusta explorar nuevos lugares y principalmente quería venir a este bosque.", "Dafne"));
+        dialogTexts.Add(new DialogData("/emote:Normal/No soy de por aquí, de hecho hace poco que vine a la ciudad por lo que no conozco mucho. Además, me gusta explorar nuevos lugares y principalmente quería venir a este bosque.", "Dafne"));
         dialogTexts.Add(new DialogData("/emote:Happy/Bueno, hice bien en traerte entonces." , "Dante"));
         dialogTexts.Add(new DialogData("/emote:Sonrojada/Si, gracias por invitarme.", "Dafne"));
-        dialogTexts.Add(new DialogData("/size:down//emote:Normal/La historia que te voy a contar es algo real que sucedió en una casa que se encuentra en este bosque. Algo que hasta la fecha nadie sabe bien que pasó. ", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Normal/La historia que te voy a contar es algo real que sucedió en una casa que se encuentra en este bosque. Algo que hasta la fecha nadie sabe bien que pasó. ", "Dante"));
         dialogTexts.Add(new DialogData("/emote:Sorpresa/¡Ohh, eso hace que se me erice la piel! /emote:Pensativa/¿Qué fue lo que sucedió?.", "Dafne"));
         dialogTexts.Add(new DialogData("/size:down//emote:Normal/Todo pasó una noche de invierno hace aproximadamente 10 años atrás. En la casa vivía una familia pudiente de mucho dinero, no se relacionaban mucho con la gente de por aquí.", "Dante"));
         dialogTexts.Add(new DialogData("/size:down//emote:Normal/En general, las personas los tenían como una familia extraña. Si bien eran amables las veces que hablaban con alguien de la ciudad, todas las personas que hablaron con ellos solían sentirse extrañamente en alerta.", "Dante"));
@@ -29,7 +29,7 @@ public class FifthSceneScript : MonoBehaviour
         dialogTexts.Add(new DialogData("/emote:Pensativa/¿Cuántos niños eran?.", "Dafne"));
         dialogTexts.Add(new DialogData("/size:down//emote:Normal/Eran dos, una niña y un niño. Él tenía 13 años y ella 12 años, siempre salían juntos. Luego estaba la esposa, que era una mujer muy hermosa y el hombre, que claramente tenía aspecto de hombre de negocios. Él tenía 47 y su mujer 44.", "Dante"));
         dialogTexts.Add(new DialogData("/emote:Pensativa/¿Y de qué trabajaban ellos?.", "Dafne"));
-        dialogTexts.Add(new DialogData("/size:down//emote:Normal/Nadie nunca lo supo en realidad, muchas de las personas de por aquí veían al hombre hablar con otros hombres trajeados. Claramente hombres de la alta sociedad.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Normal/Nadie nunca lo supo en realidad, muchas de las personas de por aquí veían al hombre hablar con otros hombres trajeados. Claramente hombres de la alta sociedad.", "Dante"));
         dialogTexts.Add(new DialogData("/size:down//emote:Normal/De la mujer aún es más misterioso, ya que nadie en general tenía la más mínima idea de lo que hacía la mujer. Se la veía mucho menos que el hombre y por lo general las veces que se la veía eran por las noches.", "Dante"));
         dialogTexts.Add(new DialogData("/emote:Pensativa/Todo esto que me cuentas luce tan extraño.", "Dafne"));
         dialogTexts.Add(new DialogData("/emote:Normal/Si, lo es y aún se pone más extraño.", "Dante"));
--- Assets/GameTest/Scripts/SixthSceneScript.cs
+++ Assets/GameTest/Scripts/SixthSceneScript.cs
@@ -6,9 +6,43 @@ using Doublsb.Dialog;
 public class SixthSceneScript : MonoBehaviour
 {
     public DialogManager DialogManager;
+    public ChangeScene ChangeSceneScript;
 
     private void Awake()
     {
+        var dialogTexts = new List<DialogData>();
 
+        //Continuación de los personajes Dafne y Dante en el Bosque.  
+        dialogTexts.Add(new DialogData("/size:down//emote:Normal/Resulta que el hombre debía irse con urgencia a un lugar de Egipto a buscar un artilugio. Esto se sabe porque un residente de la ciudad lo esuchó mientras hablaba con uno de sus amigos o socios trajeados.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Normal/También se sabe que el esposo le pidió al otro hombre que fuere lo que fuere no dejara a sus hijos desprotegidos.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Pensativa/¿Hay algo raro en esto, por qué solo los niños? ¿Qué hay de la esposa?." , "Dafne"));
+        dialogTexts.Add(new DialogData("/emote:Normal/De hecho, eso mismo se preguntaba el residente que escuchó la conversación.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Normal/.Hay muchas supersticiones al respecto de eso. Muchos dicen que el hombre trataba de cuidar a los niños de la mujer.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Pensativa/¿No se sabe que artilugio buscaba el esposo?.", "Dafne"));
+        dialogTexts.Add(new DialogData("/emote:Normal/No, pero luego me centro en eso. Por lo pronto te sigo narrando la historia.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Normal/Está bien, /wait:0.1//emote:Pensativa/muy interesante hasta ahora.", "Dafne"));
+        dialogTexts.Add(new DialogData("/emote:Normal/Luego que el esposo se haya ido a Egipto, las cosas parecían normales. Hasta que un día, un residente escuchó de madrugada que alguien gritaba en el bosque, se trataba de un grito fememino.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Normal/El hombre no se atrevió a ver que era lo que sucedía, según sus palabras, el grito era aterrador...", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Normal/Fue al bar que estaba cerca de su casa y les contó lo que había escuchado. De inmediato todos los hombres partieron hacia el bosque armados de antorchas, palos, cuchillos y escopetas.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Normal/Cuando llegaron al lugar en dónde se pretendía que había ocurrido el grito, se encontró un ciervo destrozado entero.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Normal/Los hombres quedaron horrorizados al ver la escena frente a sus ojos.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Sorpresa/¡Ayy, que horror!.", "Dafne"));
+        dialogTexts.Add(new DialogData("/emote:Normal/También encontraron un reloj de mujer. Los hombres llamaron a la policía y comenzó una investigación.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Pensativa/¿De quién era el reloj?.", "Dafne"));
+        dialogTexts.Add(new DialogData("/emote:Normal/Hasta la fecha no se ha encontrado el dueño, pero todos sabemos en esta ciudad que ese reloj pertenecía a la esposa.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Pensativa/¿Pero la policía no pudo determinar de quién era el reloj?.", "Dafne"));
+        dialogTexts.Add(new DialogData("/emote:Normal/Por supuesto que entrevistaron a cada mujer y hombre residente de la ciudad cercana al bosque. Obvio que también se entrevistó a la esposa, negó que el reloj fuera suyo...", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Pensativa/¿Y dónde estaba ella cuando ocurrieron los sucesos?.", "Dafne"));
+        dialogTexts.Add(new DialogData("/emote:Normal/Según se sabe, ella estaba durmiendo durante los sucesos. También se sabe que ella presentó coartada afirmando que el guardia de la casa nunca la vió salir.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Pensativa/Pero el guardia trabajaba para ellos, podría haber mentido.", "Dafne"));
+        dialogTexts.Add(new DialogData("/size:down//emote:Normal/Si, pero la policía aceptó su testimonio. En el lugar no se encontraron huellas ni ninguna otra pista que indicara que un ser humano estuviera en la escena, lo único que se encontró fueron huellas de animales.", "Dante"));
+        dialogTexts.Add(new DialogData("/emote:Normal/Todas las huellas fueron identificadas según la especie de animal, excepto una.","Dante"));
+        dialogTexts.Add(new DialogData("/emote:Sorpresa/¡En serio!, ¿Qué era lo raro en la huella?", "Dafne"));  
+        dialogTexts.Add(new DialogData("/size:down//emote:Normal/Los guardabosques dijeron que esa huella no se parecía a nada que hubieran visto, que se trataba de algún animal aún no descubierto o que las huellas fueron alteradas con la clara idea de borrar pistas.", "Dante")); 
+        dialogTexts.Add(new DialogData("/emote:Pensativa/Eso suena aún más terrorífico.", "Dafne"));     
+
+        DialogManager.Show(dialogTexts);
+
+        StartCoroutine(SceneChanger.WaitAndChangeScene(DialogManager, ChangeSceneScript));
     }
 }
